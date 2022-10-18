6cd2902d486442d4bc2632b2fa9a4362af855cce
Lance Zhong <lance.zhong@zendesk.com>
2022-10-18 13:05:19 -0400

    fixed not showing login error

--- timeandplace/main/views.py
+++ timeandplace/main/views.py
@@ -28,23 +28,24 @@ def register_request(request):
 
 def login_request(request):
 	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
+		try:
+			form = AuthenticationForm(request, data=request.POST)
+			# Valid the user form
+			if form.is_valid():
+				username = form.cleaned_data.get('username')
+				password = form.cleaned_data.get('password')
+				user = authenticate(username=username, password=password)
 				login(request, user)
 				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("main:index")
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
+		except:
+			messages.error(request,"Login error. Invalid username or password.")
+			return redirect("main:index")
+	else:
+		form = AuthenticationForm()
+
 	return render(request=request, template_name="main/login.html", context={"login_form":form})
 
 def logout_request(request):
 	logout(request)
-	messages.info(request, "You have successfully logged out.") 
+	messages.info(request, "You have successfully logged out.")
 	return redirect("main:index")
