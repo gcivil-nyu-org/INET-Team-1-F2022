b41a005d31b337f91c697fd798b0cda3b53379e3
Lance Zhong <lance.zhong@zendesk.com>
2022-10-20 21:07:49 -0400

    added index page

--- timeandplace/timeandplace/urls.py
+++ timeandplace/timeandplace/urls.py
@@ -19,8 +19,9 @@ from users import views as user_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path("", include("main.urls")),
+
     path('admin/', admin.site.urls),
-    # path('', views.home, name='home'),
 
     #Login and registration related routes
     path("register/", user_views.register_request, name="register"),
@@ -38,7 +39,4 @@ urlpatterns = [
 
     path("password-reset-complete", auth_views.PasswordResetCompleteView.as_view(
         template_name="users/password_reset_complete.html"), name="password_reset_complete"),
-
-
-    # path("", include("main.urls")),
 ]
