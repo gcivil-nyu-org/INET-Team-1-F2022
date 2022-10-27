fed7fef94d85c1013e6454a866f19bda2bae5bfa
Shanthi Sree Devaraju <shanthisree0315@gmail.com>
2022-10-27 16:09:54 -0400

    add tests

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Team Project repo
 
 [![Build Status](https://app.travis-ci.com/gcivil-nyu-org/INET-Team-1-F2022.svg?branch=develop)](https://app.travis-ci.com/gcivil-nyu-org/INET-Team-1-F2022)
+[![Coverage Status](https://coveralls.io/repos/github/gcivil-nyu-org/INET-Team-1-F2022/badge.svg?branch=develop)](https://coveralls.io/github/gcivil-nyu-org/INET-Team-1-F2022?branch=develop)
--- timeandplace/users/tests.py
+++ timeandplace/users/tests.py
@@ -1,3 +1,22 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+class Test_is_user_auth(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="test", password="test")
+
+    def test_user(self):
+        self.client.login(username="test", password="test")
+        assert self.user.is_authenticated
+
+    def test_user_cant_see_signup_page(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get("signup")
+        assert response.status_code == 404
+
+    def test_user_cant_see_login_page(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get("login")
+        assert response.status_code == 404
