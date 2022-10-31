fd24cf6b194813f91b3c06daced088cc10d51ece
Lance Zhong <lance.zhong@zendesk.com>
2022-10-31 15:18:18 -0400

    removed db-migate command in ebextension

---
+++ timeandplace/date/filters.py
@@ -0,0 +1,16 @@
+import django_filters
+from django_filters import CharFilter, NumericRangeFilter, RangeFilter
+
+from date.models import DummyProfile as Profile
+
+class UserFilter(django_filters.FilterSet):
+    name = CharFilter(field_name="full_name", lookup_expr="icontains")
+    gender = CharFilter(field_name="gender")
+    tech = CharFilter(field_name="tech_stack", lookup_expr='icontains')
+    age = RangeFilter(field_name="age")
+
+    class Meta:
+        model = Profile
+        fields = "__all__"
+        exclude = ["user", 'profile_image', 'bio', 'tech_stack',
+                   'full_name', 'age', 'gender', 'likeability', 'blocked_by', 'editor', 'os', 'spacing']
