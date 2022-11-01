443f4fe81c8a6833c3343bec3102a2e5f20e05f0
Kaining Mao <chrissmao99@gmail.com>
2022-11-01 13:36:04 -0400

    add restaurant.csv to data folder and modify the main/models.py

--- timeandplace/main/models.py
+++ timeandplace/main/models.py
@@ -1,3 +1,14 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+class place(models.Model):
+    DBA = models.CharField(max_length=100)
+    BORO = models.CharField(max_length=100) 
+    BUILDING = models.CharField(max_length=20)
+    STERRT = models.CharField(max_length = 40)
+    ZIPCODE = models.FloatField()
+    PHONE = models.CharField(max_length=12)
+    CUISION = models.CharField(max_length=20)
+    LATITUDE = models.FloatField()
+    LONGTITUDE = models.FloatField()
+    
