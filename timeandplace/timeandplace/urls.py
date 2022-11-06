1e7ecd15d6c7c95038f8f300a1d8c2cb5f8f0158
Lance Zhong <lance.zhong@zendesk.com>
2022-11-06 14:14:08 -0500

    bring back index page

--- timeandplace/timeandplace/urls.py
+++ timeandplace/timeandplace/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls'))
 ]
