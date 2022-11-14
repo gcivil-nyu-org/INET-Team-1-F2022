5be0254ba965304f6b47c9e7e09022a6746453ee
Lance Zhong <lance.zhong@zendesk.com>
2022-11-13 22:31:30 -0500

    fixed requirements and other view issues

--- timeandplace/account/views.py
+++ timeandplace/account/views.py
@@ -13,6 +13,7 @@ from django.db.models.query_utils import Q
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
@@ -63,32 +64,35 @@ def user_login(request):
     return render(request, 'account/login.html', {'form': form})
 
 def password_reset_request(request):
-	if request.method == "POST":
-		password_reset_form = PasswordResetForm(request.POST)
-		if password_reset_form.is_valid():
-			data = password_reset_form.cleaned_data['email']
-			associated_users = get_user_model().objects.filter(Q(email=data))
-			if associated_users.exists():
-				for user in associated_users:
-					subject = "Password Reset Requested"
-					email_template_name = "registration/password_reset_email.txt"
-					c = {
-					"email":user.email,
-					'domain':'timeandplace-dev.eba-ngz3apug.us-west-2.elasticbeanstalk.com',
-					'site_name': 'Website',
-					"uid": urlsafe_base64_encode(force_bytes(user.pk)).decode(),
-					"user": user,
-					'token': default_token_generator.make_token(user),
-					'protocol': 'http',
-					}
-					email = render_to_string(email_template_name, c)
-					try:
-						send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
-					except BadHeaderError:
-						return HttpResponse('Invalid header found.')
-					return redirect ("account/password_reset/done/")
-	password_reset_form = PasswordResetForm()
-	return render(request=request, template_name="registration/password_reset_form.html", context={"password_reset_form":password_reset_form})
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            associated_users = get_user_model().objects.filter(Q(email=data))
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "registration/password_reset_email.txt"
+                    c = {
+                    "email":user.email,
+                    'domain':'timeandplace-dev.eba-ngz3apug.us-west-2.elasticbeanstalk.com',
+                    'site_name': 'Website',
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "user": user,
+                    'token': default_token_generator.make_token(user),
+                    'protocol': 'http',
+                    }
+                    email = render_to_string(email_template_name, c)
+                    try:
+                        send_mail(subject, email, 'timeandplacenyu@gmail.com' , [user.email], fail_silently=False)
+                    except BadHeaderError:
+                        return HttpResponse('Invalid header found.')
+
+                    messages.success(request, 'A message with reset password instructions has been sent to your inbox.')
+                    return redirect ("password_reset/done/")
+            messages.error(request, 'An invalid email has been entered.')
+    password_reset_form = PasswordResetForm()
+    return render(request=request, template_name="registration/password_reset_form.html", context={"form":password_reset_form})
 
 @login_required
 def dashboard(request):
