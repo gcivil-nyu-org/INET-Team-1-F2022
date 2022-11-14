e33bf4cec351429f63bbf2c9c39d19762bb924f9
Lance Zhong <lance.zhong@zendesk.com>
2022-11-14 17:31:31 -0500

    hack forget email pass

--- timeandplace/account/urls.py
+++ timeandplace/account/urls.py
@@ -13,4 +13,10 @@ urlpatterns = [
     path('preferences/<int:pk>/', views.preferences, name="preferences"),
     path('edit_preferences/', views.edit_preferences, name='edit_preferences'),
     path('filter_profile_list/', views.filter_profile_list, name='filter_profile_list'),
+
+
+    path("password_reset", views.password_reset_request, name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
