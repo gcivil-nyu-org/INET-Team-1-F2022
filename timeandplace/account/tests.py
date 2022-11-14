14d2f216cab94ee89e34f2122f3765e92a139ab2
Shanthi Sree Devaraju <shanthigithub15>
2022-11-13 23:54:17 -0500

    test coverage

--- timeandplace/account/tests.py
+++ timeandplace/account/tests.py
@@ -42,8 +42,68 @@ class TestRegister(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+class TestLogin(TestCase):
+    def test_login_page(self):
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_process(self):
+        response = self.client.post(
+            reverse("login"),
+            data={
+                "username": "foo",
+                "password": "foobar123123ABC@"
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="test", password="test")
+        self.profile = Profile(user_id = self.user.id)
+
+    def test_home_logged_user_endpoint_returns_dashboard_page(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(path="/")
+        assert response.status_code == 200
+        self.assertTemplateUsed(response, "main/base.html")
+    
+    def test_edit_not_returns_edit_page(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(path="edit/")
+        assert response.status_code == 404
+    
+    def test_profile_list_not_returns_filter_page(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(path="profile_list/")
+        assert response.status_code == 404
+
+    def test_edit_preferences_list_not_returns_filter_page(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(path="profile_list/")
+        assert response.status_code == 404
+    
+    def test_edit_filter_list_not_returns_filter_page(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(path="filter_profile_list/")
+        assert response.status_code == 404
+
+    def test_edit_returns_edit_page(self):
+        self.client.login(username="test", password="test")
+        profile_record = Profile.objects.get(pk = self.profile.user_id)
+        user_record = User.objects.get(pk = self.user.id)
+        response = self.client.post(
+            reverse("edit"),
+            data={
+                "user_form": user_record,
+                "profile_form": profile_record
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+
+
 class TestPreferences(TestCase):
-    user = User.objects.create_user(username="test", password="test")
+    user = User.objects.create_user(username="test112345", password="test1")
     profile = Profile(user_id = user.id)
 
     def test_preferences(self):
@@ -60,10 +120,9 @@ class TestPreferences(TestCase):
 
     def test_preferences_page(self):
         url = reverse('preferences', args=[self.user.id])
-        print(url)
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'account/profile/preferences.html')
+        self.assertEqual(response.status_code, 302)
+
 
 
 
