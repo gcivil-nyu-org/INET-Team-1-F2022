970709159994bff8caf14a48f47febb33a23e14c
Lance Zhong <lance.zhong@zendesk.com>
2022-11-22 15:23:58 -0500

    change folder structure

--- timeandplace/wsgi.py
+++ timeandplace/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timeandplace/timeandplace.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timeandplace.settings')
 
 application = get_wsgi_application()
