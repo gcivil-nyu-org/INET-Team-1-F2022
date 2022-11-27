1a7f65125de3ca56e7f51e66c816085f5a21290f
Shanthi Sree Devaraju <shanthigithub15>
2022-11-27 01:36:52 -0500

    add tests to main

--- main/tests.py
+++ main/tests.py
@@ -1,27 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
-# class Test_is_user_auth(TestCase):
-#     def test_home_endpoint_returns_welcome_page(client):
-#         response = client.get(path="/")
-#         assert response.status_code == 200
-
-#     def test_login_endpoint_returns_login_page(client):
-#         response = client.get(path="login/")
-#         assert response.status_code == 200
-
-#     def test_register_endpoint_returns_login_page(client):
-#         response = client.get(path="register/")
-#         assert response.status_code == 200
-
-#     def test_password_endpoint_returns_password_page(client):
-#         response = client.get(path="password-reset/")
-#         assert response.status_code == 200
-
-#     def test_reset_endpoint_returns_reset_page(client):
-#         response = client.get(path="password-reset/done")
-#         assert response.status_code == 200
-
-#     def test_reset_endpoint_returns_reset_page(client):
-#         response = client.get(path="account/")
-#         assert response.status_code == 200
+class TestViews(TestCase):
+    def test_main_index(self):
+        url_path = ''
+        response = self.client.get(url_path)
+        self.assertEqual(response.status_code, 200)
